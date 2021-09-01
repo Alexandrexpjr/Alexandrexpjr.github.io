2db80be09a51b3ed7c40fffa12af5536e3218bc0
alexandrexpj <alexandrexpjr@hotmail.com>
2021-08-31 22:25:12 -0300

    acrescenta informaçoes sobre o ataque

--- projeto-xadrez/script.js
+++ projeto-xadrez/script.js
@@ -18,8 +18,14 @@ const whiteQueen = {
             limited: false
         },
     },
-    canMoveUnderAtack: true,
-	view: '&#9813'
+    attack: {
+        canMoveOverPieces: false,
+        attack: {
+        canMoveOverPieces: false,
+        mustMoveUnderAttack: false,
+    },
+    },
+	view: '&#9819'
 }
 
 const whiteKing = {
@@ -42,8 +48,11 @@ const whiteKing = {
             limited: true
         },
     },
-    canMoveUnderAtack: false,
-	view: '&#9812'
+    attack: {
+        canMoveOverPieces: false,
+        mustMoveUnderAttack: true,
+    },
+    view: '&#9818'
 }
 
 const whiteBishop = {
@@ -66,7 +75,11 @@ const whiteBishop = {
             limited: false
         },
     },
-	view: '&#9815'
+    attack: {
+        canMoveOverPieces: false,
+        mustMoveUnderAttack: false,
+    },
+    view: '&#9821'
 }
 
 const whiteHorse = {
@@ -77,6 +90,33 @@ const whiteHorse = {
 	color: white,
     move: {
         horizontal: {
+            isLegal: false,
+            limited: false
+        },
+        vertical: {
+            isLegal: false,
+            limited: false
+        },
+        diagonal: {
+            isLegal: false,
+            limited: false
+        },
+    },
+    attack: {
+        canMoveOverPieces: true,
+        mustMoveUnderAttack: false,
+    },
+    view: '&#9822'    
+}
+
+const whiteRook = {
+    position: {
+		line: 1,
+		column: 8
+	},
+	color: white,
+    move: {
+        horizontal: {
             isLegal: true,
             limited: false
         },
@@ -85,20 +125,52 @@ const whiteHorse = {
             limited: false
         },
         diagonal: {
-            isLegal: true,
+            isLegal: false,
             limited: false
         },
     },
-	view: '&#9813'    
+    attack: {
+        canMoveOverPieces: false,
+        mustMoveUnderAttack: false,
+    },
+    view: '&#9820;'    
 }
 
-const whiteRook = {
+const whitePawn = {
     position: {
-		line: 1,
-		column: 4
+		line: 2,
+		column: 1
 	},
 	color: white,
     move: {
+        horizontal: {
+            isLegal: false,
+            limited: false
+        },
+        vertical: {
+            isLegal: true,
+            limited: true
+        },
+        diagonal: {
+            isLegal: false,
+            limited: false
+        },
+    },
+    attack: {
+        canMoveOverPieces: false,
+        mustMoveUnderAttack: false,
+    },
+    view: '&#9823;'    
+}  
+
+
+const blackQueen = {
+    position: {
+		line: 8,
+		column: 4
+	},
+	color: black,
+    canMove: {
         horizontal: {
             isLegal: true,
             limited: false
@@ -112,33 +184,144 @@ const whiteRook = {
             limited: false
         },
     },
-	view: '&#9813'    
-}
-
-const whitePawn = {
-    
-}
-
-const blackQueen = {
-
+    attack: {
+        canMoveOverPieces: false,
+        mustMoveUnderAttack: false,
+    },
+    view: '&#9819'
 }
 
 const blackKing = {
-    
+    position: {
+		line: 8,
+		column: 5
+	},
+	color: black,
+    move: {
+        horizontal: {
+            isLegal: true,
+            limited: true
+        },
+        vertical: {
+            isLegal: true,
+            limited: true
+        },
+        diagonal: {
+            isLegal: true,
+            limited: true
+        },
+    },
+    attack: {
+        canMoveOverPieces: false,
+        mustMoveUnderAttack: true,
+    },
+    view: '&#9818'
 }
 
 const blackBishop = {
-    
+    position: {
+		line: 8,
+		column: 6
+	},
+	color: black,
+    move: {
+        horizontal: {
+            isLegal: false,
+            limited: false
+        },
+        vertical: {
+            isLegal: false,
+            limited: false
+        },
+        diagonal: {
+            isLegal: true,
+            limited: false
+        },
+    },
+    attack: {
+        canMoveOverPieces: false,
+        mustMoveUnderAttack: false,
+    },
+    view: '&#9821'
 }
 
 const blackHorse = {
-    
+    position: {
+		line: 8,
+		column: 7
+	},
+	color: black,
+    move: {
+        horizontal: {
+            isLegal: false,
+            limited: false
+        },
+        vertical: {
+            isLegal: false,
+            limited: false
+        },
+        diagonal: {
+            isLegal: false,
+            limited: false
+        },
+    },
+    attack: {
+        canMoveOverPieces: true,
+        mustMoveUnderAttack: false,
+    },
+    view: '&#9822'    
 }
 
 const blackRook = {
-    
+    position: {
+		line: 8,
+		column: 8
+	},
+	color: black,
+    move: {
+        horizontal: {
+            isLegal: true,
+            limited: false
+        },
+        vertical: {
+            isLegal: true,
+            limited: false
+        },
+        diagonal: {
+            isLegal: false,
+            limited: false
+        },
+    },
+    attack: {
+        canMoveOverPieces: false,
+        mustMoveUnderAttack: false,
+    },
+    view: '&#9820;'    
 }
 
 const blackPawn = {
-    
-}
+    position: {
+		line: 7,
+		column: 1
+	},
+	color: black,
+    move: {
+        horizontal: {
+            isLegal: false,
+            limited: false
+        },
+        vertical: {
+            isLegal: true,
+            limited: true
+        },
+        diagonal: {
+            isLegal: false,
+            limited: false
+        },
+    },
+    attack: {
+        canMoveOverPieces: false,
+        mustMoveUnderAttack: false,
+    },
+    view: '&#9823;'    
+}  
