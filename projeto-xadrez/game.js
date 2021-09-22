c704a7333f8e709a09c71b8abeb306f0faf05262
alexandrexpj <alexandrexpjr@hotmail.com>
2021-09-22 03:12:57 -0300

    adiciona movimento nar menina

--- projeto-xadrez/game.js
+++ projeto-xadrez/game.js
@@ -1,15 +1,54 @@
 const PieceColors = {
-    BLACK: 'Black',
-    WHITE: 'White'
+    WHITE: {
+        name: 'White',
+        slug: 'white',
+        direction: 1
+    },
+    BLACK: {
+        name: 'Black',
+        slug: 'black',
+        direction: -1
+    }
 };
 
+class Position {
+    constructor(x, y) {
+        this._x = x;
+        this._y = y;
+    }
+
+    toString() {
+        return `(${this._x}, ${this._y})`;
+    }
+
+    get x() {
+        return this._x;
+    }
+
+    set x(value) {
+        this._x = value;
+    }
+
+    get y() {
+        return this._y;
+    }
+
+    set y(value) {
+        this._y = value;
+    }
+
+    get name() {
+        return `${['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'][this._x]}${this._y + 1}`;
+    }
+}
+
 class Piece {
-    constructor(name, symbol, color) {
+    constructor(identifier, name, symbol, color) {
+        this._identifier = identifier;
         this._name = name;
         this._color = color;
         this._symbol = symbol;
-        this._x = 0;
-        this._y = 0;
+        this._position = new Position(0, 0);
         this._hasMoved = false;
     }
 
@@ -30,7 +69,7 @@ class Piece {
     }
 
     move(x, y) {
-        if (this.canMove()) {
+        if (this.canMove(x, y)) {
             this._hasMoved = true;
 
             this.setPosition(x, y);
@@ -47,14 +86,18 @@ class Piece {
     setPosition(x, y) {
         if (x < 0 || x > 7 || y < 0 || y > 7) return false;
 
-        this._x = x;
-        this._y = y;
+        this.position._x = x;
+        this.position._y = y;
 
         return true;
     }
 
+    get identifier() {
+        return this._identifier;
+    }
+
     get name() {
-        return `${this._color} ${this._name}`;
+        return `${this._color.name} ${this._name}`;
     }
 
     get symbol() {
@@ -62,19 +105,15 @@ class Piece {
     }
 
     get position() {
-        return `(${this._x}, ${this._y})`;
-    }
-
-    get positionName() {
-        return `${['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'][this._x]}${this._y + 1}`;
+        return this._position;
     }
 
     get x() {
-        return this._x;
+        return this._position.x;
     }
 
     get y() {
-        return this._y;
+        return this._position.y;
     }
 
     get color() {
@@ -82,63 +121,141 @@ class Piece {
     }
 }
 
-class Pawn extends Piece {
-    constructor(color) {
-        super('Pawn', '&#9823', color);
-    }
-
-    validMoves() {
-        return [];
-    }
-}
-
 class King extends Piece {
-    constructor(color) {
-        super('King', '&#9818', color);
+    constructor(identifier, color) {
+        super(identifier, 'King', '&#9818', color);
     }
 
     validMoves() {
-        return [];
+        let validMoves = [
+            new Position(this.x + 1, this.y),
+            new Position(this.x - 1, this.y),
+            new Position(this.x + 1, this.y + 1),
+            new Position(this.x - 1, this.y + 1),
+            new Position(this.x + 1, this.y - 1),
+            new Position(this.x - 1, this.y - 1),
+            new Position(this.x, this.y + 1),
+            new Position(this.x, this.y - 1)
+        ];
+
+        // Removendo quem tá fora do tabuleiro
+        validMoves = validMoves.filter((position) => (position.x >= 0 && position.x < 8 && position.y >= 0 && position.y < 8));
+        
+        return validMoves;
     }
 }
 
 class Queen extends Piece {
-    constructor(color) {
-        super('Queen', '&#9819', color);
+    constructor(identifier, color) {
+        super(identifier, 'Queen', '&#9819', color);
     }
-
+    
     validMoves() {
-        return [];
-    }
-}
+        let validMoves = [];
 
-class Horse extends Piece {
-    constructor(color) {
-        super('Horse', '&#9822', color);
-    }
+        for (let i = 1; i <= 7; i += 1) {
+            validMoves.push(new Position(this.x + i, this.y), 
+                new Position(this.x - i, this.y), 
+                new Position(this.x, this.y - i), 
+                new Position(this.x, this.y + i), 
+                new Position(this.x + i, this.y + i), 
+                new Position(this.x - i, this.y - i), 
+                new Position(this.x + i, this.y - i), 
+                new Position(this.x - i, this.y + i));
+        }
 
-    validMoves() {
-        return [];
+        // Removendo quem tá fora do tabuleiro
+        validMoves = validMoves.filter((position) => (position.x >= 0 && position.x < 8 && position.y >= 0 && position.y < 8));
+
+        return validMoves;
     }
 }
 
 class Rook extends Piece {
-    constructor(color) {
-        super('Rook', '&#9820', color);
+    constructor(identifier, color) {
+        super(identifier, 'Rook', '&#9820', color);
     }
 
     validMoves() {
-        return [];
+        let validMoves = [];
+
+        for (let i = 1; i <= 7; i += 1) {
+            validMoves.push(new Position(this.x + i, this.y), 
+                new Position(this.x - i, this.y), 
+                new Position(this.x, this.y - i), 
+                new Position(this.x, this.y + i));
+        }
+
+        // Removendo quem tá fora do tabuleiro
+        validMoves = validMoves.filter((position) => (position.x >= 0 && position.x < 8 && position.y >= 0 && position.y < 8));
+
+        return validMoves;
     }
 }
 
 class Bishop extends Piece {
-    constructor(color) {
-        super('Bishop', '&#9821', color);
+    constructor(identifier, color) {
+        super(identifier, 'Bishop', '&#9821', color);
     }
 
     validMoves() {
-        return [];
+        let validMoves = [];
+
+        for (let i = 1; i <= 7; i += 1) {
+            validMoves.push(new Position(this.x + i, this.y + i), 
+                new Position(this.x - i, this.y - i), 
+                new Position(this.x + i, this.y - i), 
+                new Position(this.x - i, this.y + i));
+        }
+
+        // Removendo quem tá fora do tabuleiro
+        validMoves = validMoves.filter((position) => (position.x >= 0 && position.x < 8 && position.y >= 0 && position.y < 8));
+
+        return validMoves;
+    }
+}
+
+class Horse extends Piece {
+    constructor(identifier, color) {
+        super(identifier, 'Horse', '&#9822', color);
+    }
+
+    validMoves() {
+        let validMoves = [
+            new Position(this.x - 2, this.y + 1),
+            new Position(this.x - 1, this.y + 2),
+            new Position(this.x + 1, this.y + 2),
+            new Position(this.x + 2, this.y + 1),
+            new Position(this.x + 2, this.y - 1),
+            new Position(this.x + 1, this.y - 2),
+            new Position(this.x - 1, this.y - 2),
+            new Position(this.x - 2, this.y - 1)
+        ];
+        
+        // Removendo quem tá fora do tabuleiro
+        validMoves = validMoves.filter((position) => (position.x >= 0 && position.x < 8 && position.y >= 0 && position.y < 8));
+
+        return validMoves;
+    }
+}
+
+class Pawn extends Piece {
+    constructor(identifier, color) {
+        super(identifier, 'Pawn', '&#9823', color);
+    }
+
+    validMoves() {
+        let validMoves = [
+            new Position(this.x, this.y + this._color.direction)    
+        ];
+
+        // Passo duplo inicial
+        if (!this._hasMoved) validMoves.push(new Position(this.x, this.y + (this._color.direction * 2)));
+        
+        // Removendo quem tá fora do tabuleiro
+        validMoves = validMoves.filter((position) => (position.x >= 0 && position.x < 8 && position.y >= 0 && position.y < 8));
+
+        return validMoves;
     }
 }
 
@@ -150,8 +267,8 @@ class ChessBoard {
 
     initialize() {
         // Kings
-        const whiteKing = new King(PieceColors.WHITE);
-        const blackKing = new King(PieceColors.BLACK);
+        const whiteKing = new King('wk', PieceColors.WHITE);
+        const blackKing = new King('bk', PieceColors.BLACK);
 
         whiteKing.setPosition(4, 0);
         blackKing.setPosition(4, 7);
@@ -159,8 +276,8 @@ class ChessBoard {
         this._pieces.push(whiteKing, blackKing);
 
         // Queens
-        const whiteQueen = new Queen(PieceColors.WHITE);
-        const blackQueen = new Queen(PieceColors.BLACK);
+        const whiteQueen = new Queen('wq', PieceColors.WHITE);
+        const blackQueen = new Queen('bq', PieceColors.BLACK);
 
         whiteQueen.setPosition(3, 0);
         blackQueen.setPosition(3, 7);
@@ -168,10 +285,10 @@ class ChessBoard {
         this._pieces.push(whiteQueen, blackQueen);
 
         // Rooks
-        const whiteRookQ = new Rook(PieceColors.WHITE);
-        const whiteRookK = new Rook(PieceColors.WHITE);
-        const blackRookQ = new Rook(PieceColors.BLACK);
-        const blackRookK = new Rook(PieceColors.BLACK);
+        const whiteRookQ = new Rook('wrq', PieceColors.WHITE);
+        const whiteRookK = new Rook('wrk', PieceColors.WHITE);
+        const blackRookQ = new Rook('brq', PieceColors.BLACK);
+        const blackRookK = new Rook('brk', PieceColors.BLACK);
 
         whiteRookQ.setPosition(0, 0);
         whiteRookK.setPosition(7, 0);
@@ -181,10 +298,10 @@ class ChessBoard {
         this._pieces.push(whiteRookQ, whiteRookK, blackRookQ, blackRookK);
 
         // Bishop
-        const whiteBishopQ = new Bishop(PieceColors.WHITE);
-        const whiteBishopK = new Bishop(PieceColors.WHITE);
-        const blackBishopQ = new Bishop(PieceColors.BLACK);
-        const blackBishopK = new Bishop(PieceColors.BLACK);
+        const whiteBishopQ = new Bishop('wbq', PieceColors.WHITE);
+        const whiteBishopK = new Bishop('wbk', PieceColors.WHITE);
+        const blackBishopQ = new Bishop('bbq', PieceColors.BLACK);
+        const blackBishopK = new Bishop('bbk', PieceColors.BLACK);
 
         whiteBishopQ.setPosition(2, 0);
         whiteBishopK.setPosition(5, 0);
@@ -194,10 +311,10 @@ class ChessBoard {
         this._pieces.push(whiteBishopQ, whiteBishopK, blackBishopQ, blackBishopK);
 
         // Horse
-        const whiteHorseQ = new Horse(PieceColors.WHITE);
-        const whiteHorseK = new Horse(PieceColors.WHITE);
-        const blackHorseQ = new Horse(PieceColors.BLACK);
-        const blackHorseK = new Horse(PieceColors.BLACK);
+        const whiteHorseQ = new Horse('whq', PieceColors.WHITE);
+        const whiteHorseK = new Horse('whk', PieceColors.WHITE);
+        const blackHorseQ = new Horse('bhq', PieceColors.BLACK);
+        const blackHorseK = new Horse('bhk', PieceColors.BLACK);
 
         whiteHorseQ.setPosition(1, 0);
         whiteHorseK.setPosition(6, 0);
@@ -207,9 +324,11 @@ class ChessBoard {
         this._pieces.push(whiteHorseQ, whiteHorseK, blackHorseQ, blackHorseK);
         
         // Pawns
-        for (let x = 0; x < 8; x += 1) {
-            const whitePawn = new Pawn(PieceColors.WHITE);
-            const blackPawn = new Pawn(PieceColors.BLACK);
+        for (let i = 1; i <= 8; i += 1) {
+            const whitePawn = new Pawn(`wp${i}`, PieceColors.WHITE);
+            const blackPawn = new Pawn(`bp${i}`, PieceColors.BLACK);
+
+            const x = i - 1;
 
             whitePawn.setPosition(x, 1);
             blackPawn.setPosition(x, 6);
--- projeto-xadrez/gui.js
+++ projeto-xadrez/gui.js
@@ -2,21 +2,33 @@ const chessBoardEl = document.querySelector('#chessBoard');
 const gridLayerEl = document.querySelector('#gridLayer');
 const piecesLayerEl = document.querySelector('#piecesLayer');
 
+const chessBoard = new ChessBoard();
+let selectedPiece = undefined;
+
 function createBoard() {
   for (let i = 0; i < 64; i += 1) {
+    const y = Math.floor(i / 8);
+    const x = i - (y * 8);
+    const positionName = `${['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'][x]}${8 - y}`;
+
     const squareEl = document.createElement('div');
-    squareEl.classList.add('square');
+    squareEl.classList.add('square', positionName);
     gridLayerEl.appendChild(squareEl);
   }
 }
 
 function createPiece(piece) {
   const pieceEl = document.createElement('div');
-  pieceEl.classList.add((piece.color === PieceColors.WHITE) ? 'white' : 'black');
-  pieceEl.classList.add(piece.positionName);
-  pieceEl.classList.add('piece');
+  pieceEl.classList.add('piece', piece.identifier, piece.color.slug, piece.position.name);
   pieceEl.innerHTML = piece.symbol;
+  pieceEl.addEventListener('click', onPieceClick(piece));
   piecesLayerEl.appendChild(pieceEl);
+}
+
+function updatePiece(piece) {
+  const pieceEl = piecesLayerEl.querySelector(`.piece.${piece.identifier}`);
+  pieceEl.className = '';
+  pieceEl.classList.add('piece', piece.identifier, piece.color.slug, piece.position.name);
 }
 
 function createPieces(chessBoard) {
@@ -25,7 +37,80 @@ function createPieces(chessBoard) {
   });
 }
 
-const chessBoard = new ChessBoard();
+function deselectAllSquares() {
+  const selectedSquares = gridLayerEl.querySelectorAll('.square.selected');
+  selectedSquares.forEach((squareEl) => {
+    squareEl.classList.remove('selected');
+  });
+}
+
+function removeMoveHighlights() {
+  const movesSquares = gridLayerEl.querySelectorAll('.square.can-move');
+  movesSquares.forEach((squareEl) => {
+    squareEl.classList.remove('can-move');
+  });
+}
+
+function highlightMoves(positions) {
+  positions.forEach((position) => {
+    const squareEl = gridLayerEl.querySelector(`.square.${position.name}`);
+    squareEl.classList.add('can-move');
+  });
+}
+
+function selectSquare(piece) {
+  selectedPiece = piece;
+
+  // removeMoveHighlights();
+
+  const validMoves = selectedPiece.validMoves();
+  highlightMoves(validMoves);
+
+  const squareEl = gridLayerEl.querySelector(`.square.${selectedPiece.position.name}`);
+  squareEl.classList.add('selected');
+}
+
+function movePiece(piece, x, y) {
+  console.log('MOVE');
+  piece.move(x, y);
+  updatePiece(piece);
+  removeMoveHighlights();
+}
+
+function onPieceClick(piece) {
+  return (event) => {
+    deselectAllSquares();
+    selectSquare(piece);
+  };
+}
+
+function onChessBoardClick(event) {
+  if (selectedPiece) {
+    const squareSize = chessBoardEl.offsetWidth / 8;
+
+    let layerX = event.offsetX || event.layerX;
+    let layerY = event.offsetY || event.layerY;
+
+    if (event.target.classList.contains('piece')) {
+      layerX += event.target.offsetLeft;
+      layerY += event.target.offsetTop;
+    }
+
+    const x = Math.floor(layerX / squareSize);
+    const y = 7 - Math.floor(layerY / squareSize);
+
+    // Só chama o movimento se o alvo for diferente dele meixmo
+    if (selectedPiece.x != x || selectedPiece.y != y) {
+      movePiece(selectedPiece, x, y);
+    }
+  }
+}
+
+
+chessBoardEl.addEventListener('click', onChessBoardClick);
+
+// Game
+
 chessBoard.initialize();
 
 createBoard();
